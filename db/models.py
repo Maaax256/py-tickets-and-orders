ed0b131eba479dd67fec366be3bd6722bed3862f
Maksym Omelkov <omelkov85@gmail.com>
2025-03-28 05:39:21 +0200

    Tickets and orders (1.0)

--- db/models.py
+++ db/models.py
@@ -22,6 +22,11 @@ class Movie(models.Model):
     actors = models.ManyToManyField(to=Actor, related_name="movies")
     genres = models.ManyToManyField(to=Genre, related_name="movies")
 
+    class Meta:
+        indexes = [
+            models.Index(fields=["title"])
+        ]
+
     def __str__(self) -> str:
         return self.title
 
